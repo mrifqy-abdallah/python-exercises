192f5ab6b40cdc10482ac1e36bc9a3f3e60d6bb3
rifqy abdallah <mrifqyabdallah@gmail.com>
2021-04-27 14:31:33 +0700

    finding scores in a list

---
+++ easy/4. high-scores/high_scores.py
@@ -0,0 +1,11 @@
+def latest(scores):  # `scores` is a list
+    return scores[-1]
+
+
+def personal_best(scores):
+    return max(scores)
+
+
+def personal_top_three(scores):
+    scores.sort(reverse = True)
+    return scores[:3]
