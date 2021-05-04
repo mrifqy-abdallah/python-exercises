b8927466531ab6055f98fa1cbf0807fce42bb7f3
rifqy abdallah <mrifqyabdallah@gmail.com>
2021-05-04 14:53:12 +0700

    adding code's complexity

--- medium/001_twelve-days/twelve_days.py
+++ medium/001_twelve-days/twelve_days.py
@@ -1,29 +1,44 @@
-def recite(start_verse, end_verse):
-    the_song = [
-        # 1
-        "On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.",
-        # 2
-        "On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.",
-        # 3
-        "On the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
-        # 4
-        "On the fourth day of Christmas my true love gave to me: four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
-        # 5
-        "On the fifth day of Christmas my true love gave to me: five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
-        # 6
-        "On the sixth day of Christmas my true love gave to me: six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
-        # 7
-        "On the seventh day of Christmas my true love gave to me: seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
-        # 8
-        "On the eighth day of Christmas my true love gave to me: eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
-        # 9
-        "On the ninth day of Christmas my true love gave to me: nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
-        # 10
-        "On the tenth day of Christmas my true love gave to me: ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
-        # 11
-        "On the eleventh day of Christmas my true love gave to me: eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
-        # 12
-        "On the twelfth day of Christmas my true love gave to me: twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
+def recite(start_verse: int, end_verse: int):
+    result = []
+    for i in range(start_verse, end_verse + 1):
+        print(collect_verse(i))
+        result.append(collect_verse(i))
+    print(result)
+    return result
+
+
+def collect_verse(verse_index):
+    main_verse = [
+        "a Partridge in a Pear Tree.",
+        "two Turtle Doves, and ",
+        "three French Hens, ",
+        "four Calling Birds, ",
+        "five Gold Rings, ",
+        "six Geese-a-Laying, ",
+        "seven Swans-a-Swimming, ",
+        "eight Maids-a-Milking, ",
+        "nine Ladies Dancing, ",
+        "ten Lords-a-Leaping, ",
+        "eleven Pipers Piping, ",
+        "twelve Drummers Drumming, "
     ]
 
-    return the_song[start_verse-1:end_verse]
+    days = {
+        1: "first", 
+        2: "second",
+        3: "third",
+        4: "fourth",
+        5: "fifth",
+        6: "sixth",
+        7: "seventh",
+        8: "eighth",
+        9: "ninth",
+        10: "tenth",
+        11: "eleventh",
+        12: "twelfth"
+    }
+
+    whole_verse = "On the " + days[verse_index] + " day of Christmas my true love gave to me: "
+    for i in range(verse_index, 0, -1):
+        whole_verse += main_verse[i-1]
+    return whole_verse
