e454338f776019ea150b0bc705a523df977f53d4
rifqy abdallah <mrifqyabdallah@gmail.com>
2021-05-18 16:40:33 +0700

    implementing bitwise operator '&'

--- easy/054_secret-handshake/secret_handshake.py
+++ easy/054_secret-handshake/secret_handshake.py
@@ -1,29 +1,7 @@
 def commands(number: int):
-    result = []
-    binary = int(format(number, "b"))
+    handshakes = ["wink", "double blink", "close your eyes", "jump"]
+    result = [handshake for i, handshake in enumerate(handshakes) if 2**i & number]
 
-    # The instruction is asking to check the binary backwards
-    # Here the checking is forward, therefore the result needs to be reversed by default
-    reverse = True
-
-    while binary > 0:
-        if binary >= 10000:
-            reverse = False
-            binary %= 10000
-        elif binary >= 1000:
-            result.append("jump")
-            binary %= 1000
-        elif binary >= 100:
-            result.append("close your eyes")
-            binary %= 100
-        elif binary >= 10:
-            result.append("double blink")
-            binary %= 10
-        elif binary == 1:
-            result.append("wink")
-            binary %= 1
-    
-    if reverse:
+    if 2**4 & number:
         result.reverse()
-
     return result
